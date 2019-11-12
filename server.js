e4a3c686e1a7e47e7db9c6324276b1242117841f
a.nguyen525i@gmail.com <a.nguyen525i@gmail.com>
2019-11-11 21:31:30 -0700

    got scraper and notes working...need handlebars

--- server.js
+++ server.js
@@ -1,67 +1,107 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var mongojs = require("mongojs");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
 var app = express();
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
 
+var PORT = 3000;
 
-var db = mongojs(databaseUrl, collections);
+var db = require("./models");
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
-db.on("error", function(error) {
-    console.log("Database Error:", error);
-});
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
 
-app.get("/", function(req, res) {
-    res.send("Hello world");
-});
-
-app.get("/all", function(req, res) {
-
-    db.scrapedData.find( {}, function(error, found) {
-        if(error) {
-            console.log(error);
-        }else{
-            res.json(found);
-        }
-    });
-
-});
 
 
 app.get("/scrape", function(req, res) {
 
+    db.Article.remove( {}, function(err) {
+
+        if(err) {
+            throw error;
+        } else {
+            res.send("Deleted previous scrape collection.");
+        }
+
+    });
+    
+   
     axios.get("http://www.espn.com/espn/latestnews").then(function(response) {
 
         var $ = cheerio.load(response.data);
 
         $("li").each( function(i, element) {
-            var title = $(element).children("a").attr("title");
-            var link = $(element).children("a").attr("href");
+        
+            var result = {};
 
-            if (title && link) {
-
-                db.scrapedData.insert( {
-
-                    title: title,
-                    link: link
-                }, 
-                function(err, inserted){
-                    if(err) {
-                        console.log(err);
-                    }else{
-                        console.log(inserted);
-                    }
+            result.title = $(this).children("a").attr("title");
+            result.link = $(this).children("a").attr("href");
+            result.summary = $(this).text();
+            
+            db.Article.create(result)
+                .then(function(dbArticle){
+                    console.log(dbArticle);
+                })
+                .catch(function(err){
+                    console.log(err);
                 });
-            }
+
         });
-    });
 
     res.send("Scrape Complete");
+
+    });
 });
 
-app.listen(3000, function() {
-    console.log("App running on port 3000.");
+
+app.get("/all", function(req, res) {
+
+    db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+
+app.get("/all/:id", function(req, res) {
+
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.post("/all/:id", function(req, res) {
+
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.findOneAndUpdate( { _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT);
 });
